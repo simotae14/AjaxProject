14a44aa6269da1849a292f0365b1872ddf76bdd2
Simone Taeggi <startae14@gmail.com>
2018-02-12 01:15:00 +0100

    Ajax Workshop part1
    aggiunte le funzioni per creare il menu

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(function(){
+	var containerEl = $('#main-menu');
 	function handleMenu() {
 		function toggleInfoPanel(anchor) {
 			var infoPanelEl = $(this).siblings('.menu-item-info');
@@ -8,4 +9,26 @@ $(function(){
 			.mouseover(toggleInfoPanel)
 			.mouseout(toggleInfoPanel);
 	}
+
+	function buildMenuItem(data) {
+		var liEl,
+			infoPanelEl;
+
+		liEl = $('<li class="menu-item">' + 
+									'<a href="#">' + data.title + '</a>' + 
+									'<div class="menu-item-info"></div>' + 
+							'</li>');
+		return liEl;
+	}
+
+	$.get('api/menu.json', function(data){
+		// i dati son stati caricati
+
+		// cancello il contenuto dell'ul
+		containerEl.empty();
+
+		data.forEach(function(menuItem){
+			containerEl.append(buildMenuItem(menuItem));	
+		});
+	}, 'json');
 });
